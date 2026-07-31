cf4015805a442b12f0f5f14e356b48f26d1fa95d
DRodFS <jrodriguezbel@unal.edu.co>
2026-07-30 21:32:43 -0400

    Changed emotion_detection return

--- emotion_detector.py
+++ emotion_detector.py
@@ -1,8 +1,25 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
-    @URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     HEADERS = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(URL, json = myobj, headers=HEADERS)
-    return response.text
+    formatted_response = json.loads(response.text)
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+
+    max_score = 0
+    max_emotion = None
+    for emotion, score in emotions.items():
+        if score > max_score:
+            max_score = score
+            max_emotion = emotion
+    
+    result = {
+            **emotions,  # Desempaqueta todas las emociones
+            'dominant_emotion': max_emotion,
+        }
+
+
+    return result
